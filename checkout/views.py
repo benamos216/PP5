e8c9c4fb9b5f9ee15a355b42232697d1d1eb0b34
benamos216 <benamos216@gmail.com>
2023-02-05 17:46:40 +0000

    styled checkout page to show items in basket

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
+from basket.contexts import basket_contents
+from stock.models import Stock
+
 
 def checkout(request):
 
+    if request.method == 'POST':
+        basket = request.session.get('basket', {})
+
     template = 'checkout/checkout.html'
     context = {
-        
+
     }
 
     return render(request, template, context)
