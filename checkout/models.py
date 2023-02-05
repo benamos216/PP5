b1c4372e98d10633d895204376836e619ce722c1
benamos216 <benamos216@gmail.com>
2023-02-05 21:31:34 +0000

    continued developing order form

--- checkout/models.py
+++ checkout/models.py
@@ -10,12 +10,11 @@ from user_details.models import UserDetails
 class Order(models.Model):
 
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
+    user_profile = models.ForeignKey(UserDetails, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -70,4 +69,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.stock.sku} on order {self.order.order_number}
+        return f'SKU {self.stock.sku} on order {self.order.order_number}'
