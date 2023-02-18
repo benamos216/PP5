d2d79304a085acf4049577196a7a25f94e35fd5b
benamos216 <benamos216@gmail.com>
2023-02-18 15:17:37 +0000

    finished profile and order history views

--- checkout/admin.py
+++ checkout/admin.py
@@ -14,7 +14,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'original_basket',
                        'grand_total',  'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county',
