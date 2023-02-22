ab71543bc6a13707366af4d1ee956cede006ca51
benamos216 <benamos216@gmail.com>
2023-02-22 20:39:41 +0000

    configuring emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -25,13 +25,15 @@ class StripeWH_Handler:
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-        
+
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )        
+        )
+
+        print(body)
 
     def handle_event(self, event):
         """
